2f52a011dd61ea4ef2c740fd3d0d6653f5a267d5
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 <sabaudianin@gmail.com>
2025-01-20 18:22:31 +0100

    updated newsletter to connect with slice fn

--- src/components/newsletter/NewsLetter.jsx
+++ src/components/newsletter/NewsLetter.jsx
@@ -1,9 +1,17 @@
-import { useState } from "react";
 import { IoSend } from "react-icons/io5";
 import { useNewsletterForm } from "../../hooks/useNewsLetterForm";
 
 export const NewsLetter = () => {
-  const { register, handleSubmit, errors, onSubmit } = useNewsletterForm();
+  const {
+    register,
+    handleSubmit,
+    errors,
+    onSubmit,
+    loading,
+    error,
+    exist,
+    added,
+  } = useNewsletterForm();
 
   return (
     <section className="py-16 lg:py-24 bg-indigo-100 dark:bg-neutral-900 flex justify-center items-center flex-col ">
@@ -31,16 +39,34 @@ export const NewsLetter = () => {
             <button
               type="submit"
               className="bg-white p-5 text-indigo-500"
+              disabled={loading}
             >
               <IoSend />
             </button>
           </form>
+
           <span className="text-xxs p-1">
             By providing your email address you accept the Privacy Policy.
           </span>
           {errors.email && (
             <p className="text-xl text-shadow font-semibold">
               {errors.email.message}
+            </p>
+          )}
+
+          {error && (
+            <p className="text-red-600 text-xl font-semibold mt-2">{error}</p>
+          )}
+
+          {exist && (
+            <p className="text-red-500 text-xl font-semibold mt-2">
+              This email is already in our newsletter!
+            </p>
+          )}
+
+          {added && (
+            <p className="text-green-500 text-xl font-semibold mt-2">
+              Thank you for joining our newsletter!
             </p>
           )}
         </div>
--- src/hooks/useNewsLetterForm.js
+++ src/hooks/useNewsLetterForm.js
@@ -1,6 +1,12 @@
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { newsletterSchema } from "../validations/newsletterSchema";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  checkEmailExist,
+  addEmail,
+  resetSuccess,
+} from "../redux/slices/emailSlice";
 
 export const useNewsletterForm = () => {
   const {
@@ -12,8 +18,19 @@ export const useNewsletterForm = () => {
     resolver: zodResolver(newsletterSchema),
   });
 
-  const onSubmit = (data) => {
+  const dispatch = useDispatch();
+  const { loading, error, exist, added } = useSelector(
+    (state) => state.newsletter
+  );
+
+  const onSubmit = async (data) => {
     console.log(data);
+    const result = await dispatch(checkEmailExist(data.email));
+    if (result.payload.exist) {
+      return;
+    }
+    await dispatch(addEmail(data.email));
+    resetSuccess();
     reset();
   };
 
@@ -22,5 +39,9 @@ export const useNewsletterForm = () => {
     handleSubmit,
     errors,
     onSubmit,
+    loading,
+    error,
+    exist,
+    added,
   };
 };
