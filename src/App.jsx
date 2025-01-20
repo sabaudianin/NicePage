1bf54d2e4e4e8db880c7767e03646f4f3059d025
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 <sabaudianin@gmail.com>
2025-01-20 14:19:30 +0100

    Fixed icon/href % Bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AppLayout } from "./components/layout/AppLayout.jsx";
 import { Home } from "./components/home/Home.jsx";
-import { About } from "./pages/about/About.jsx";
 import { Tickets } from "./pages/tickets/Tickets.jsx";
 import { ThemeInitializer } from "./theme/ThemeInitializer.js";
 import { NotFound } from "./utils/NotFound.jsx";
