40d48efb2520cd5502540d97b83f574406c0ebd5
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 <sabaudianin@gmail.com>
2025-01-30 18:08:40 +0100

    SingleQUote refactor

--- src/components/recomendation/SingleQuote.jsx
+++ src/components/recomendation/SingleQuote.jsx
@@ -1,3 +1,5 @@
+import { FaCircle } from "react-icons/fa";
+
 export const SingleQuote = ({ quote, imgSrc, name, title }) => {
   return (
     <figure className="mt-10">
@@ -12,19 +14,10 @@ export const SingleQuote = ({ quote, imgSrc, name, title }) => {
         />
         <div className="mt-4 flex items-center justify-center space-x-3 text-base">
           <div className="font-semibold text-gray-900">{name}</div>
-          <svg
-            width={10}
-            height={10}
-            viewBox="0 0 2 2"
-            aria-hidden="true"
-            className="fill-black"
-          >
-            <circle
-              r={1}
-              cx={1}
-              cy={1}
-            />
-          </svg>
+          <FaCircle
+            size={10}
+            className="text-black"
+          />
           <div className="text-gray-600">{title}</div>
         </div>
       </figcaption>
